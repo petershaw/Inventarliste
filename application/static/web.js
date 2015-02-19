51e8860208d5d9be5391100ac77e3f459f8b792a
Kris <kris@opentabs.de>
2015-02-19 21:03:51 +0100

    cleanup
    remove unused comments

--- application/static/web.js
+++ application/static/web.js
@@ -1,5 +1,5 @@
 /**
- * User: Hannes Moser <hannes@impossiblearts.com>
+ * 
  */
 require.config({
 
@@ -36,31 +36,10 @@ require.config({
         , "underscore"          : "../components/underscore/underscore"
         , "backbone"            : "../components/backbone/backbone"
         , "text"                : "../components/requirejs-text/text"
-
-// 		"asyncjs"			  : "libs/async/lib/async",
-// 
-//         // Utils
-//         "jquery.validate"     : "../plugins/jquery-validation-1.11.1/jquery.validate",
-//         "jquery.browser"	  : "libs/jquery.browser/dist/jquery.browser.min",
-// 
-//         // UI
-//         "bootstrap"                     : "../plugins/bootstrap/js/bootstrap",
-//         "jquery-ui"                     : "../plugins/jquery-ui-1.10.3/jqueryui",
-//         "jquery.ui.widget"              : "libs/jquery-file-upload/js/vendor/jquery.ui.widget",
-//         // migrate to moment in the future?
-//         "moment"                        : "libs/moment/moment",
-//         // data-tables
-//         "data-table"                    : "../plugins/data-tables/jquery.dataTables",
-//         "data-table-bootstrap"          : "../plugins/data-tables/DT_bootstrap",
-//         "templates"						: "./templates"
     },
 
     "priority": [
-        "jquery",
-//         "jquery-ui",
-//         "jquery-ui-widget",
-//         "data-table",
-//         "data-table-bootstrap"
+        "jquery"
     ]
 
 });
